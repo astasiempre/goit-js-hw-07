efed14e484a230d6282e1092ec9e6a44bcf661aa
astasiempre <ero1494651@gmail.com>
2023-07-21 15:05:36 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery")
+
+const markup = galleryItems.map(({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join("");
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+gallery.addEventListener("click", onClick)
+
+function onClick(event) {
+  event.preventDefault();
+  if (event.target.classList.contains('gallery__image'));
+  const largeImgUrl = event.target.dataset.source;
+  openModal(largeImgUrl)
+}
+function openModal(url) {
+  const instance = basicLightbox.create(`<img src="${url}" width="800" height="600">`);
+  instance.show();
+  }  
+console.log(markup);
